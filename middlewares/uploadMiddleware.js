e195c4e0de40b76f89b2b92bb7fe66dd0d508dcf
mr-mashanlo <mr.mashanlo@yandex.ru>
2024-07-29 09:52:58 +0500

    update upload middleware

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,11 +1,5 @@
 const multer = require( 'multer' );
 const path = require( 'path' );
-const fs = require( 'fs' );
-
-const uploadsDir = path.join( __dirname, '../uploads/' );
-if ( !fs.existsSync( uploadsDir ) ) {
-  fs.mkdirSync( uploadsDir, { recursive: true } );
-}
 
 const storage = multer.diskStorage( {
   destination: function ( req, file, cb ) {
