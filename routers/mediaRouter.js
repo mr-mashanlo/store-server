ab8708fadc972fffb0b2d8af14163c85739c8df9
mr-mashanlo <mr.mashanlo@yandex.ru>
2024-08-09 13:22:07 +0500

    refactor media router

--- routers/mediaRouter.js
+++ routers/mediaRouter.js
@@ -2,11 +2,11 @@ const Router = require( 'express' );
 const router = new Router();
 const authMiddleware = require( '../middlewares/authMiddleware' );
 
-const mediaController = require( '../controllers/media/mediaController.js' );
-const uploadMiddleware = require( '../middlewares/uploadMiddleware.js' );
+// const mediaController = require( '../controllers/media/mediaController.js' );
+// const uploadMiddleware = require( '../middlewares/uploadMiddleware.js' );
 
-// const mediaController = require( '../controllers/media/vercelMediaController.js' );
-// const uploadMiddleware = require( '../middlewares/vercelUploadMiddleware.js' );
+const mediaController = require( '../controllers/media/vercelMediaController.js' );
+const uploadMiddleware = require( '../middlewares/vercelUploadMiddleware.js' );
 
 router.get( '/all', authMiddleware, mediaController.getAll );
 router.post( '/', authMiddleware, uploadMiddleware, mediaController.upload );
